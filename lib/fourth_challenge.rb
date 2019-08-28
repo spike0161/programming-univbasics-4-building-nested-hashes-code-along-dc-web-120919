837e0f519ab5c22dc7bb19b6caa2ebf9dbdefc9c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-28 19:45:46 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [:name => "Benvolio", :age => "17", :attitude => "worried"
         #your hashes here!
       ]
    }, 
@@ -13,7 +13,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+       :name => "Mercutio", :age => "18", :attitude => "hot-headed"
       ]
    }
 }
